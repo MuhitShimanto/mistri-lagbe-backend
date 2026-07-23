cbbe038430d1504738fdbcd49781f37928b9d293
Muhit Shimanto <muhit.shimanto02@gmail.com>
2026-07-23 21:37:49 +0600

    fix: api documentation file response error

--- src/modules/docs/docs.controller.ts
+++ src/modules/docs/docs.controller.ts
@@ -1,13 +1,15 @@
-import type {Request, Response, NextFunction} from "express";
+import type { Request, Response, NextFunction } from 'express';
+import path from 'path';
 
 class DocsController {
-    apiDocs = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            // Return the html content of the API documentation
-            return res.sendFile("api-docs.html", { root: "src/modules/docs" });
-        } catch (error) {
-            next(error);
-        }
+  apiDocs = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Return the html content of the API documentation
+      const filePath = path.join(process.cwd(), 'src', 'modules', 'docs', 'api-docs.html');
+      return res.sendFile(filePath);
+    } catch (error) {
+      next(error);
     }
+  };
 }
 export default new DocsController();
