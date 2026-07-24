d50b1294783b755487906e821dfd013c1abc96c4
Muhit Shimanto <muhit.shimanto02@gmail.com>
2026-07-24 16:12:31 +0600

    fix: review submission error due to false condition

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -7,8 +7,9 @@ import type { CreateReviewInput } from './review.validation.js';
 class ReviewService {
   createReview = async (data: CreateReviewInput, user: User) => {
     // Check if the booking belongs to the user and if the booking is completed
+    console.log(data)
     const booking = await bookingService.getBookingById(data.bookingId, user.id);
-    if (!booking || booking.customerId !== user.id || booking.status !== BookingStatus.PAID) {
+    if (!booking || booking.customerId !== user.id || booking.status !== BookingStatus.COMPLETED) {
       throw new ApiError(400, 'Invalid booking ID or booking is not completed');
     }
 
